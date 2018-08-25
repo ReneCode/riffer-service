9eb8ec9029f220a5fad379348cf21d6f0b6e9f38
rene_mbp <harryloveslearning@googlemail.com>
2018-08-25 19:26:53 +0200

    port change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log('hello world')
 
 let http = require('http');
-let port = 64001
+let port = 64002
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
