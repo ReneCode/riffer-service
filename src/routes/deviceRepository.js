443167cf22ca2035342f26e53670e6b53abd3716
rene_mbp <harryloveslearning@googlemail.com>
2018-10-03 21:29:23 +0200

    some device-init-data

--- src/routes/deviceRepository.js
+++ src/routes/deviceRepository.js
@@ -2,7 +2,15 @@ const uuidv4 = require("uuid/v4");
 
 class DeviceRepository {
   constructor() {
-    this.devices = [{ id: 1, name: "abc" }, { id: 2, name: "xyz" }];
+    this.devices = [
+      { id: 1, name: "=A1+O1-F1" },
+      { id: 2, name: "=A1+O1-F2", connectionDescription: ["1", "2"] },
+      {
+        id: 2,
+        name: "=A1+O2-M3",
+        connectionDescription: ["U1", "V1", "W1", "U2", "V2", "W2"]
+      }
+    ];
   }
 
   getAll() {
